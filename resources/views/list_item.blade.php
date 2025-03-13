e7ef39e94f42fe7ae7df93b5e45867996c6394a4
nelifauziyah88 <nelifauziyahh@gmail.com>
2025-03-13 10:50:10 +0700

    Menambahkan Controller dan View untuk Login, Dashboard, dan List Item

---
+++ resources/views/list_item.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>List Item</title>
+</head>
+<body>
+    <h1>Halaman List Item</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,47 @@
 <?php
 
+use App\Http\Controllers\ListBarangController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ListItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// routes/web.php
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+// routes/web.php
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+});
+
+// routes/web.php
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
+
+
+// Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+//    return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+
+// Route web php pbl
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/list_item', [ListItemController::class, 'index']);
